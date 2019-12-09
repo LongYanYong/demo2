c4177f5106abcde47505d91e68aae07b9e72c2fa
LongYanYong <928799532@qq.com>
2019-12-10 00:29:29 +0800

    change

--- src/views/pages/Form.vue
+++ src/views/pages/Form.vue
@@ -1,48 +1,83 @@
 <template>
-		<el-form :model="form" label-width="80px" ref="form">
-			<el-form-item label="活动名称">
-				<el-input v-model="form.name" placeholder="请输入活动名称"></el-input>
-			</el-form-item>
-			<el-form-item label="活动区域">
-				<el-select v-model="form.region" placeholder="请选择活动区域">
-					<el-option label="区域一" value="上海"></el-option>
-					<el-option label="区域二" value="北京"></el-option>
-				</el-select>
-			</el-form-item>
-			<el-form-item label="活动时间">
-				<el-col :span="11">
-					<el-date-picker type="date" placeholder="请选择日期" v-model="form.date1" style="width: 100%"></el-date-picker>
-				</el-col>
-				<el-col class="line" :span="2">-</el-col>
-				<el-col :span="11">
-					<el-time-picker placeholder="请选择时间" v-model="form.date2" style="width: 100%"></el-time-picker>
-				</el-col>
-			</el-form-item>
-			<el-form-item label="即时配送">
-				<el-switch v-model="form.delivery"></el-switch>
-			</el-form-item>
-			<el-form-item label="活动性质">
-				<el-checkbox-group v-model="form.type">
-					<el-checkbox label="美食/餐厅线上活动" name="type"></el-checkbox>
-					<el-checkbox label="地推活动" name="type"></el-checkbox>
-					<el-checkbox label="线下主题活动" name="type"></el-checkbox>
-					<el-checkbox label="单纯品牌曝光" name="type"></el-checkbox>
-				</el-checkbox-group>
-			</el-form-item>
-			<el-form-item label="特殊资源">
-				<el-radio-group v-model="form.resource">
-					<el-radio label="线上品牌商赞助"></el-radio>
-					<el-radio label="线下场地免费"></el-radio>
-				</el-radio-group>
-			</el-form-item>
-			<el-form-item label="活动形式">
+		<div>
+			<el-form :model="form" label-width="80px" ref="form">
+				<el-form-item label="活动名称">
+					<el-input v-model="form.name" placeholder="请输入活动名称"></el-input>
+				</el-form-item>
+				<el-form-item label="活动区域">
+					<el-select v-model="form.region" placeholder="请选择活动区域">
+						<el-option label="区域一" value="上海"></el-option>
+						<el-option label="区域二" value="北京"></el-option>
+					</el-select>
+				</el-form-item>
+				<el-form-item label="活动时间">
+					<el-col :span="11">
+						<el-date-picker type="date" placeholder="请选择日期" v-model="form.date1" style="width: 100%"></el-date-picker>
+					</el-col>
+					<el-col class="line" :span="2">-</el-col>
+					<el-col :span="11">
+						<el-time-picker placeholder="请选择时间" v-model="form.date2" style="width: 100%"></el-time-picker>
+					</el-col>
+				</el-form-item>
+				<el-form-item label="即时配送">
+					<el-switch v-model="form.delivery"></el-switch>
+				</el-form-item>
+				<el-form-item label="活动性质">
+					<el-checkbox-group v-model="form.type">
+						<el-checkbox label="美食/餐厅线上活动" name="type"></el-checkbox>
+						<el-checkbox label="地推活动" name="type"></el-checkbox>
+						<el-checkbox label="线下主题活动" name="type"></el-checkbox>
+						<el-checkbox label="单纯品牌曝光" name="type"></el-checkbox>
+					</el-checkbox-group>
+				</el-form-item>
+				<el-form-item label="特殊资源">
+					<el-radio-group v-model="form.resource">
+						<el-radio label="线上品牌商赞助"></el-radio>
+						<el-radio label="线下场地免费"></el-radio>
+					</el-radio-group>
+				</el-form-item>
+				<el-form-item label="活动形式">
 					<el-input type="textarea" v-model="form.desc"></el-input>
-			</el-form-item>
-			<el-form-item>
-				<el-button type="primary" @click="onSubmit">立即创建</el-button>
-				<el-button>取消</el-button>
-			</el-form-item>
-		</el-form>
+				</el-form-item>
+				<el-form-item>
+					<el-button type="primary" @click="onSubmit">立即创建</el-button>
+					<el-button>取消</el-button>
+				</el-form-item>
+			</el-form>
+			<el-form :inline="true" :model="formInline">
+				<el-form-item label="审批人">
+					<el-input v-model="formInline.user" placeholder="审批人"></el-input>
+				</el-form-item>
+				<el-form-item label="活动区域">
+						<el-select v-model="formInline.region" placeholder="活动区域">
+							<el-option label="区域一" value="shanghai"></el-option>
+							<el-option label="区域二" value="北京"></el-option>
+						</el-select>
+				</el-form-item>
+				<el-form-item>
+					<el-button type="" @click="onQuery">查询</el-button>
+				</el-form-item>
+			</el-form>
+			<el-form>
+				<el-radio-group v-model="labelPosition" size="small">
+					<el-radio-button label="left">左对齐</el-radio-button>
+					<el-radio-button label="right">右对齐</el-radio-button>
+					<el-radio-button label="top">顶部对齐</el-radio-button>
+				</el-radio-group>
+			</el-form>
+			<div style="margin: 20px"></div>
+			<el-form label-width="80px" :label-position="labelPosition" :model="formLabelAlign">
+				<el-form-item label="名称">
+					<el-input v-model="formLabelAlign.name"></el-input>
+				</el-form-item>
+				<el-form-item label="活动区域">
+					<el-input v-model="formLabelAlign.region"></el-input>
+				</el-form-item>
+				<el-form-item label="活动形式">
+					<el-input v-model="formLabelAlign.type"></el-input>
+				</el-form-item>
+			</el-form>
+		</div>
 </template>
 
 <script>
@@ -50,6 +85,7 @@
         name: "Form",
 		    data() {
             return {
+                labelPosition: 'right',
                 form: {
                     name: '',
                     region:'',
@@ -59,12 +95,24 @@
 		                type: [],
                     resource:'',
                     desc: ''
+                },
+                formInline: {
+                    user: '',
+                    region: ''
+                },
+                formLabelAlign: {
+		                name: '',
+                    region: '',
+                    type: ''
                 }
             }
 		    },
 		    methods: {
             onSubmit() {
                 console.log('submit');
+            },
+            onQuery() {
+                console.log('点击了按钮');
             }
 		    }
     }
---
+++ src/views/pages/formTabs.vue
@@ -0,0 +1,24 @@
+<template>
+	<div>
+		<el-tabs>
+			<el-tab-pane>
+
+			</el-tab-pane>
+		</el-tabs>
+	</div>
+</template>
+
+<script>
+    export default {
+        name: "formTabs",
+		    data() {
+            return {
+
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
